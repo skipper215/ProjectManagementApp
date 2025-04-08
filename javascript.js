9d78ee5eae2ac834efe23e19a445290b9c645e67
Ivan Ma <ivanma215@gmail.com>
2025-04-08 21:44:36 +1000

    changed toDoList array of strings to an array of objects to accomadate addition of date per todo

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 
-const toDoList = JSON.parse(localStorage.getItem("todos")) || [];
+const toDoList = JSON.parse(localStorage.getItem("todos")) || [{
+    name: "",
+    dueDate: ""
+}];
+
 window.onload = displayToDos;
 
 function addToDo() {
     const inputElement = document.querySelector(".js-input");
-    toDoList.push(inputElement.value);
+    const inputDateElement = document.querySelector(".js-date")
+    toDoList.push({
+        name: inputElement.value,
+        dueDate: inputDateElement.value
+    });
     inputElement.value = ""; //clears search box
     localStorage.setItem("todos", JSON.stringify(toDoList))
     console.log(toDoList);
@@ -26,9 +34,12 @@ function displayToDos() {
     const todoElement = document.querySelector(".todos")
     todoElement.innerHTML = "";  // clear previous iteration of display
     for (let i=0; i<toDoList.length; i++) {
+        const toDoObject = toDoList[i]
+        const name = toDoObject.name;
+        const date = toDoObject.dueDate;
         todoElement.innerHTML += 
         `<li class='toDoItem'> 
-        <input type='checkbox'>${toDoList[i]}
+        <input type='checkbox'>${name} ${date}
         <button onclick="removeToDo(${i})" class='js-remove'>Remove</button> 
         </li>`;
     }
