51538620168da2ffe0590220b9da120c1bed0b8d
Ivan Ma <ivanma215@gmail.com>
2025-04-09 14:52:26 +1000

    added checkbox functionality to complete a toDo

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ const toDoList = JSON.parse(localStorage.getItem("todos")) || [{
     name: "",
     dueDate: ""
 }];
+let completedToDoList = []; 
 
 window.onload = displayToDos;
 
@@ -39,9 +40,48 @@ function displayToDos() {
         const date = toDoObject.dueDate;
         todoElement.innerHTML += 
         `<li class='toDoItem'> 
-        <input type='checkbox'>${name} ${date}
+        <input class= 'checkbox-todo' type='checkbox' data-index=${i}>${name} ${date}
         <button onclick="removeToDo(${i})" class='js-remove'>Remove</button> 
         </li>`;
     }
+    // check checkboxes
+    const checkboxes = document.querySelectorAll(".checkbox-todo");
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener("change", () => {
+            console.log("Checkbox state changed!");  // Debugging line
+            const index = Number(checkbox.dataset.index);
+            const checkedToDo = toDoList[index];
+            if (checkbox.checked) {
+                completedToDoList.push(checkedToDo);
+                removeToDo(index);
+            } else {
+                //remove it from completedToDoList
+                completedToDoList = completedToDoList.filter(item => item !== checkedToDo); //keep all items 'as long as' != toDoObject
+                
+                console.log("removed: " + completedToDoList);
+            }
+            displayCompletedToDos();
+            
+        })
+    })
+
+
+
+    
 }
 
+function displayCompletedToDos() {
+    // add completedToDos to html
+    const completedToDoElement = document.querySelector(".completedToDoList");
+    completedToDoElement.innerHTML =""; //remove previous instance of completedToDoList display
+    for (let i=0; i<completedToDoList.length; i++) {
+        const name = completedToDoList[i].name;
+        console.log(completedToDoList[i]);
+
+        completedToDoElement.innerHTML +=
+        `<li class="completedToDoItem">
+            ${name}
+        </li>
+        `
+    }
+}
